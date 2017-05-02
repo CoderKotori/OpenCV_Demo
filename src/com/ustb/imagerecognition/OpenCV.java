7e6185e7c9060702041584106513e7f372f5e3ad
CoderKotori <947069773@qq.com>
2017-05-02 10:40:51 +0800

    first demo: change image channel to grey

--- src/com/ustb/imagerecognition/OpenCV.java
+++ src/com/ustb/imagerecognition/OpenCV.java
@@ -4,6 +4,8 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 /**
  * Created by Coder on 2017/4/25.
@@ -15,5 +17,10 @@ public class OpenCV {
     public static void main(String[]args) {
         Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("m = " + m.dump());
+
+        String fileLocation = "image/test.jpg";
+        Mat image = Imgcodecs.imread(fileLocation);
+        Imgproc.cvtColor(image, image, Imgproc.COLOR_BGR2GRAY);
+        Imgcodecs.imwrite("image_dispose/result.jpg", image);
     }
 }
